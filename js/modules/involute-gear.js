34e562f22f787ed60fe03af833ce9e9bbd646679
Ryo Suzuki <ryosuzk@gmail.com>
2015-10-17 18:26:10 -0600

    Update

--- js/modules/involute-gear.js
+++ js/modules/involute-gear.js
@@ -1,1 +1,76 @@
-involute-gear.js
+function involuteGear(numTeeth, circularPitch, pressureAngle, clearance, thickness)
+{
+  // default values:
+  if(arguments.length < 3) pressureAngle = 20;
+  if(arguments.length < 4) clearance = 0;
+  if(arguments.length < 4) thickness = 1;
+
+  var addendum = circularPitch / Math.PI;
+  var dedendum = addendum + clearance;
+
+  // radiuses of the 4 circles:
+  var pitchRadius = numTeeth * circularPitch / (2 * Math.PI);
+  var baseRadius = pitchRadius * Math.cos(Math.PI * pressureAngle / 180);
+  var outerRadius = pitchRadius + addendum;
+  var rootRadius = pitchRadius - dedendum;
+
+  var maxtanlength = Math.sqrt(outerRadius*outerRadius - baseRadius*baseRadius);
+  var maxangle = maxtanlength / baseRadius;
+
+  var tl_at_pitchcircle = Math.sqrt(pitchRadius*pitchRadius - baseRadius*baseRadius);
+  var angle_at_pitchcircle = tl_at_pitchcircle / baseRadius;
+  var diffangle = angle_at_pitchcircle - Math.atan(angle_at_pitchcircle);
+  var angularToothWidthAtBase = Math.PI / numTeeth + 2*diffangle;
+
+  // build a single 2d tooth in the 'points' array:
+  var resolution = 5;
+  var points = [new CSG.Vector2D(0,0)];
+  for(var i = 0; i <= resolution; i++)
+  {
+    // first side of the tooth:
+    var angle = maxangle * i / resolution;
+    var tanlength = angle * baseRadius;
+    var radvector = CSG.Vector2D.fromAngle(angle);
+    var tanvector = radvector.normal();
+    var p = radvector.times(baseRadius).plus(tanvector.times(tanlength));
+    points[i+1] = p;
+
+    // opposite side of the tooth:
+    radvector = CSG.Vector2D.fromAngle(angularToothWidthAtBase - angle);
+    tanvector = radvector.normal().negated();
+    p = radvector.times(baseRadius).plus(tanvector.times(tanlength));
+    points[2 * resolution + 2 - i] = p;
+  }
+
+  // create the polygon and extrude into 3D:
+  var tooth3d = new CSG.Polygon2D(points).extrude({offset: [0, 0, thickness]});
+
+  var allteeth = new CSG();
+  for(var j = 0; j < numTeeth; j++)
+  {
+    var ang = j*360/numTeeth;
+    var rotatedtooth = tooth3d.rotateZ(ang);
+    allteeth = allteeth.unionForNonIntersecting(rotatedtooth);
+  }
+
+  // build the root circle:
+  points = [];
+  var toothAngle = 2 * Math.PI / numTeeth;
+  var toothCenterAngle = 0.5 * angularToothWidthAtBase;
+  for(var k = 0; k < numTeeth; k++)
+  {
+    var angl = toothCenterAngle + k * toothAngle;
+    var p1 = CSG.Vector2D.fromAngle(angl).times(rootRadius);
+    points.push(p1);
+  }
+
+  // create the polygon and extrude into 3D:
+  var rootcircle = new CSG.Polygon2D(points).extrude({offset: [0, 0, thickness]});
+
+  var result = rootcircle.union(allteeth);
+
+  // center at origin:
+  result = result.translate([0, 0, -thickness/2]);
+
+  return result;
+}
